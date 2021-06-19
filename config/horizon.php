64fed1a16f206fb5eef2f26fe57961102d196983
Matt Wolf <probie93@aol.com>
2021-06-19 02:18:10 -0400

    Removing Totem

--- config/horizon.php
+++ config/horizon.php
@@ -9,6 +9,7 @@ return [
     | Horizon Domain
     |--------------------------------------------------------------------------
     |
+    |
     | This is the subdomain where Horizon will be accessible from. If this
     | setting is null, Horizon will reside under the same domain as the
     | application. Otherwise, this value will serve as the subdomain.
